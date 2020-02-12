35ef0f0e06e24af10d2a2da124b8552c493d6d56
Alex Liang <aliang@atd-us.com>
2020-02-12 15:04:13 -0500

    cleaning up some things

--- frontend/src/app/home/welcome.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './welcome.component.html'
-})
-export class WelcomeComponent {
-  public pageTitle = 'Welcome to GPT-2 demo';
-}
